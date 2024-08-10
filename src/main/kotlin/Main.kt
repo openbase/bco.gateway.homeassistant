14e6534024817179c0eaccb23ebe910d00252a66
lhuxohl <zeephyros@gmail.com>
2024-08-10 18:50:39 +0200

    make compileable

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,7 @@
 package org.example
 
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.runBlocking
-import org.example.util.toRequest
 import org.example.websocket.WebsocketClient
 
 fun main() {
     val client = WebsocketClient()
-
-    runBlocking {
-        client.query(mapOf("id" to 19, "type" to "ping").toRequest())
-        client.subscribe(mapOf("id" to 18, "type" to "subscribe_events").toRequest()).collect()
-    }
 }

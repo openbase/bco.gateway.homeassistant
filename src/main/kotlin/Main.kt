bd1992ac91ba21f8f24b23ed94ddb2c1cc419b6a
lhuxohl <zeephyros@gmail.com>
2024-08-10 16:59:51 +0200

    add websocket subscription

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,15 @@
 package org.example
 
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.runBlocking
+import org.example.util.toRequest
 import org.example.websocket.WebsocketClient
 
 fun main() {
-    WebsocketClient()
+    val client = WebsocketClient()
+
+    runBlocking {
+        client.query(mapOf("id" to 19, "type" to "ping").toRequest())
+        client.subscribe(mapOf("id" to 18, "type" to "subscribe_events").toRequest()).collect()
+    }
 }
--- src/main/kotlin/websocket/WebsocketClient.kt
+++ src/main/kotlin/websocket/WebsocketClient.kt
@@ -3,16 +3,16 @@ package org.example.websocket
 import io.ktor.client.*
 import io.ktor.client.engine.java.*
 import io.ktor.client.plugins.websocket.*
-import io.ktor.client.request.*
 import io.ktor.websocket.*
-import kotlinx.coroutines.isActive
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
 import org.example.util.toRequest
 import org.slf4j.LoggerFactory
 import java.net.http.HttpClient
 
 class WebsocketClient {
-    private val TOKEN: String = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhY2M5MDdlNDg3MTg0YmMwOGQ1MzI3MGUyNGNiYmNmYSIsImlhdCI6MTcyMzI4OTEzMCwiZXhwIjoyMDM4NjQ5MTMwfQ.h94N7dFk9052oe6I5BsYJ3fVmBWoxTW3yXonhgwGhxU"
+    private val token: String = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhY2M5MDdlNDg3MTg0YmMwOGQ1MzI3MGUyNGNiYmNmYSIsImlhdCI6MTcyMzI4OTEzMCwiZXhwIjoyMDM4NjQ5MTMwfQ.h94N7dFk9052oe6I5BsYJ3fVmBWoxTW3yXonhgwGhxU"
 
     private val log = LoggerFactory.getLogger(javaClass)
 
@@ -26,13 +26,11 @@ class WebsocketClient {
 
     private val authRequest = mapOf(
         "type" to "auth",
-        "access_token" to TOKEN
+        "access_token" to token
     ).toRequest()
 
-    init {
-        runBlocking {
-            query(mapOf("id" to 19, "type" to "ping").toRequest())
-        }
+    fun close() {
+        client.close()
     }
 
     suspend fun query(message: String): String? {
@@ -55,9 +53,7 @@ class WebsocketClient {
         return result
     }
 
-    suspend fun subscribe(message: String): List<String?> {
-        val result: MutableList<String?> = emptyList<String?>().toMutableList()
-
+    fun subscribe(message: String): Flow<String?> = flow {
         client.webSocket(host = "scummbar", port = 8123, path = "/api/websocket") {
             logMessage((incoming.receive() as? Frame.Text)?.readText())
             send(authRequest)
@@ -73,12 +69,10 @@ class WebsocketClient {
 
             while (true) {
                 val temp = (incoming.receive() as? Frame.Text)?.readText()
-                result.addLast(temp)
                 logMessage(temp)
+                emit(temp)
             }
         }
-
-        return result
     }
 
     private fun logMessage(message: String?) = runBlocking {

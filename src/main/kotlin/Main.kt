8f156882f975f4baf21df057f6a60588d8201977
lhuxohl <zeephyros@gmail.com>
2024-08-10 16:46:22 +0200

    create web socket client and send ping request

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,7 @@
 package org.example
 
-import io.ktor.client.*
-import io.ktor.client.call.*
-import io.ktor.client.engine.java.*
-import io.ktor.client.request.*
-import kotlinx.coroutines.runBlocking
+import org.example.websocket.WebsocketClient
 
 fun main() {
-    val client = HttpClient(Java) {
-        engine {
-            pipelining = true
-            protocolVersion = java.net.http.HttpClient.Version.HTTP_2
-        }
-    }
-
-     runBlocking {
-         val response = client.get("http://scummbar:8123/api/config") {
-            bearerAuth("")
-            headers {
-                append("Content-Type", "application/json")
-            }
-        }
-
-        println(response.body<String>())
-    }
+    WebsocketClient()
 }

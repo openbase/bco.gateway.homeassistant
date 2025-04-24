c64fb7372025b3270cb6317f331b49bcdb1ea248
LukBelter <lukbelter@openbase.org>
2025-04-24 22:15:34 +0200

    add requested changes

--- src/main/kotlin/org/openbase/bco/device/hass/manager/service/TemperatureStateServiceImpl.kt
+++ src/main/kotlin/org/openbase/bco/device/hass/manager/service/TemperatureStateServiceImpl.kt
@@ -1,12 +1,17 @@
 package org.openbase.bco.device.hass.manager.service
-
+import org.openbase.bco.dal.lib.layer.service.provider.TemperatureStateProviderService
+import org.openbase.bco.dal.lib.layer.unit.Unit
 import org.openbase.bco.device.hass.manager.dto.HassDeviceClass
-import org.openbase.bco.device.hass.manager.dto.HassStateAttributes
 import org.openbase.bco.device.hass.manager.dto.HassStateDto
 import org.openbase.type.domotic.state.TemperatureStateType.TemperatureState
 
+class TemperatureStateServiceImpl<ST>(unit: ST) : HassService<ST>(unit),
+    TemperatureStateProviderService where ST : TemperatureStateProviderService, ST : Unit<*> {
+    override fun getTemperatureState(): TemperatureState = unit.temperatureState
+    }
+
 fun HassStateDto.isTemperatureSensor() : Boolean =
-    attributes[HassStateAttributes.DEVICE_CLASS.id] == HassDeviceClass.TEMPERATURE.id
+    deviceClass == HassDeviceClass.TEMPERATURE.id
 
 
 fun HassStateDto.toTemperatureState(): TemperatureState.Builder {

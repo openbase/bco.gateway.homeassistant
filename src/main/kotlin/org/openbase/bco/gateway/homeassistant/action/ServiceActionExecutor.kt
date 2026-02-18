babfaa45553b146dbc94bba161a6d6e8af88d13e
lhuxohl <zeephyros@gmail.com>
2026-02-18 20:26:22 +0100

    remove unused imports

--- src/main/kotlin/org/openbase/bco/gateway/homeassistant/action/ServiceActionExecutor.kt
+++ src/main/kotlin/org/openbase/bco/gateway/homeassistant/action/ServiceActionExecutor.kt
@@ -1,14 +1,12 @@
 package org.openbase.bco.gateway.homeassistant.action
 
 import com.google.gson.JsonObject
-import org.openbase.bco.dal.control.layer.unit.AbstractUnitController
 import org.openbase.bco.dal.lib.action.ActionDescriptionProcessor
 import org.openbase.bco.gateway.homeassistant.manager.cache.HassIdToUnitControllerCache
 import org.openbase.bco.gateway.homeassistant.manager.dto.HassStateDto
-import org.openbase.bco.gateway.homeassistant.manager.service.*
 import org.openbase.bco.gateway.homeassistant.manager.transformer.toServiceState
 import org.openbase.bco.gateway.homeassistant.manager.transformer.toServiceType
-import org.openbase.bco.gateway.homeassistant.util.*
+import org.openbase.bco.gateway.homeassistant.util.parse
 import org.openbase.jul.exception.CouldNotPerformException
 import org.openbase.jul.exception.InvalidStateException
 import org.openbase.jul.pattern.Observer
@@ -17,7 +15,6 @@ import org.openbase.type.domotic.action.ActionInitiatorType.ActionInitiator
 import org.openbase.type.domotic.action.ActionInitiatorType.ActionInitiator.InitiatorType
 import org.openbase.type.domotic.action.ActionPriorityType.ActionPriority
 import org.openbase.type.domotic.service.ServiceTemplateType.ServiceTemplate.ServiceType
-import org.openbase.type.vision.ColorType.*
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.util.concurrent.TimeUnit
